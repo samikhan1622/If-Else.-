ae8803515cfc14ddd78fa3bb9062d0b3c238da7a
Sami Ullah <153662652+samikhan1622@users.noreply.github.com>
2024-02-25 14:40:01 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,81 @@
+//Q#01 (conditions)
+// let studendGander =  "female";
+// if(studendGander === "male"){
+//     console.log("ap ki entry left side he");
+// }
+// else{
+//     console.log("ap ki enty right side he");
+// }
+// let salary = 40000
+// let monthlyexpense = 80000
+// if (salary==40000) {
+//     console.log("you have not enough money");
+// } else {
+//     console.log("you have enough money"); 
+// }
+// console.log("taking decision");
+// let raita = false 
+// let biscuit = true
+// if (raita) {
+//     console.log("buy samosa");
+// }
+// else if  (biscuit){
+//     console.log("buy biscuit");
+// }
+// else {
+//     console.log("dont buy");
+// }
+// console.log("taking desicion");
+// let budget = 40000
+// let cycle = 30000
+// if (cycle <= budget) {
+//     console.log("buy cycle");    
+// }
+// else {
+//     console.log(" i need to build to earn more");
+// }
+// //single = assign
+// let num = 2
+// console.log(num);
+// //double == match
+// console.log(num == 2);
+// //triple === match - data type
+// console.log(num === 2);
+//second example
+//single = assign
+// let num:any = "2"
+// let username = "sami"
+// console.log(num);
+// console.log(num == 2 );
+// let age= 14
+// if (age <=10){
+//     console.log("sorry you kid you can't drive ");
+// }
+// else if(age<18){
+//     console.log("you are under 18 you cant drive");
+// }
+// else {
+//     console.log("you can drive");
+// }
+function addMoney(shakeel, sami) {
+    return shakeel + sami;
+}
+console.log(addMoney(40, 60));
+//nested (if, else, statment)
+// let country = "pakistan"
+// let age = 20
+// if (country === "pakistan") {
+//     if(age >= 18) {
+//         console.log("here your ticket");
+//     }else {
+//         console.error("age restricted");
+//     }
+// }
+// else {
+//     console.log("invalid country");
+// }
+//function simple..
+function shakeelKi(sami, aqif, saqib) {
+    return sami + aqif + saqib;
+}
+console.log(shakeelKi(50, 30, 10));
